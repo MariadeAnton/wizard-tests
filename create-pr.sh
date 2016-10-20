2913c4156784efe5e7f16421e1951c4873d39ea8
mariadeanton <mariadeanton@gmail.com>
2016-10-20 14:17:33 +0200

    create magic PR

--- create-pr.sh
+++ create-pr.sh
@@ -1,24 +1,8 @@
 #! /usr/bin/env bash
 
-title='{
-  "Magic Pull Request"
-}'
-head='{
-  "add-travis-ci-support"
-}'
-base='{
-  "main"
-}'
-body='{
-  "the content of my PR goes here, I think"
-}'
-
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token OAUTH-TOKEN" \
-  -d "$title" \
-  -d "$head" \
-  -d "$base" \
-  -d "$body" \
+  -H "Authorization: token AUTH_TOKEN" \
+  -d '{"body":"the content of my PR goes here, I think", "title":"Magic Pull Request", "head":"test", "base":"main"}' \
   https://api.github.com/repos/MariadeAnton/wizard-tests/pulls
